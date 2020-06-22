861157a38a05b43c48cfa4dfec0879dbb1ef1eb4
Oscar Megía López <pfgytk@gmail.com>
2020-06-22 11:29:25 +0200

    Modificada la barra superior de menús

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -39,20 +39,9 @@ AppAsset::register($this);
         'options' => ['class' => 'navbar-nav navbar-right'],
         'items' => [
             ['label' => 'Home', 'url' => ['/site/index']],
-            ['label' => 'About', 'url' => ['/site/about']],
-            ['label' => 'Contact', 'url' => ['/site/contact']],
-            Yii::$app->user->isGuest ? (
-                ['label' => 'Login', 'url' => ['/site/login']]
-            ) : (
-                '<li>'
-                . Html::beginForm(['/site/logout'], 'post')
-                . Html::submitButton(
-                    'Logout (' . Yii::$app->user->identity->username . ')',
-                    ['class' => 'btn btn-link logout']
-                )
-                . Html::endForm()
-                . '</li>'
-            )
+            ['label' => 'Importar', 'url' => ['/site/importar']],
+            ['label' => 'Generar test directamente', 'url' => ['/site/generardirectamente']],
+            ['label' => 'Generar test por categoría', 'url' => ['/site/generarcategoria']],
         ],
     ]);
     NavBar::end();
@@ -69,7 +58,7 @@ AppAsset::register($this);
 
 <footer class="footer">
     <div class="container">
-        <p class="pull-left">&copy; My Company <?= date('Y') ?></p>
+        <p class="pull-left">&copy; Oscar Megía López 2020</p>
 
         <p class="pull-right"><?= Yii::powered() ?></p>
     </div>
